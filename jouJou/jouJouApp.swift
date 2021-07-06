73207a07cdd1c1c0652b183c71aa4ac5041c4f7b
Dara Caroline <daracaroline@MacBook-Pro-de-Dara.local>
2021-07-06 17:00:51 -0300

    adicionando o projeto

---
+++ jouJou/jouJouApp.swift
@@ -0,0 +1,20 @@
+//
+//  jouJouApp.swift
+//  jouJou
+//
+//  Created by Dara Caroline on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct jouJouApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
