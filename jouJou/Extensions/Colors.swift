fd7d18b39a0daa5dbba2ef85d2ecf86e594ba82b
Evaldo Júnior <evalldojn@gmail.com>
2021-07-13 15:58:17 -0300

    corrigindo adicionar fontes e cores

--- jouJou/Extensions/Colors.swift
+++ jouJou/Extensions/Colors.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+extension Color {
+    static let blackColor = Color(UIColor(named:"blackColor")!)
+    static let beigeColor = Color(UIColor(named:"beigeColor")!)
+    static let blueColor = Color(UIColor(named:"blueColor")!)
+    static let greenColor = Color(UIColor(named:"greenColor")!)
+    static let orangeColor = Color(UIColor(named:"orangeColor")!)
+    static let darkSalmonColor = Color(UIColor(named:"darkSalmonColor")!)
+    static let lightSalmonColor = Color(UIColor(named:"lightSalmonColor")!)
+}
